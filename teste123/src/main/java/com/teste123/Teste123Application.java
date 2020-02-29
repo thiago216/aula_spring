661cfd580fb0f8ac87ef013fa1dfcaeede83f541
Aluno <Aluno@LAB0620>
2020-02-28 21:37:12 -0300

    inclusão dos arquivos iniciais do projeto

---
+++ teste123/src/main/java/com/teste123/Teste123Application.java
@@ -0,0 +1,13 @@
+package com.teste123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teste123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teste123Application.class, args);
+	}
+
+}
---
+++ teste123/src/main/java/com/teste123/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.teste123.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public ModelAndView início() {
+		ModelAndView mv = new ModelAndView("produto/CadProduto");
+		return mv;
+	}
+}
